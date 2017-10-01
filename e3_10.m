82e257299acceb9d56921d91fe223a202d601da1
Sam <sam.witt25@gmail.com>
2017-10-01 13:26:04 +0200

    3.10 af

--- e3_10.m
+++ e3_10.m
@@ -1,5 +1,18 @@
-clear, clf
+clear, clc
 
+k = 0:20
+h = 10.^(-1.*k)
+diff = ((9+h).^0.5-9^0.5)./h
+trunc = diff-(1/(2*9^0.5))
 
+format shortG
+headings = {'k', 'h', 'diff', 'trunc'}
+
+tk = k'
+th = h'
+tdiff = diff'
+ttrunc = trunc'
+
+disp(table(tk, th, tdiff, ttrunc, 'VariableNames', headings))
 
 
