4a5635ce6f7833fe4261474850aa33e69ef6d81d
Sam <sam.witt25@gmail.com>
2017-10-01 13:55:57 +0200

    4 start

---
+++ e4_1.m
@@ -0,0 +1,12 @@
+clear, clc
+
+i   = atan(pi/4)+cos(asin(pi/4))
+ii  = exp(1)
+iii = [exp(5)*exp(-2)-exp(-3) exp(5)+exp(-2)-exp(3)]
+iv  = log(0.5)+log(4)-log(2)
+v   = [log(0) atan(inf)]
+vi  = 1
+
+
+
+
