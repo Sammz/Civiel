1e8c2d70c8a9e9379b7ec5148fcada680b904c72
Sam <sam.witt25@gmail.com>
2017-10-04 17:17:39 +0200

    4.1 af

--- e4_1.m
+++ e4_1.m
@@ -5,7 +5,12 @@ ii  = exp(1)
 iii = [exp(5)*exp(-2)-exp(-3) exp(5)+exp(-2)-exp(3)]
 iv  = log(0.5)+log(4)-log(2)
 v   = [log(0) atan(inf)]
-vi  = 1
+vi  = [sqrt(abs(-2)) sqrt(-2)]
+vii = [factorial(10) factorial(10)/(factorial(2)*factorial(5))]
+
+
+
+
 
 
 
