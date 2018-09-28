9fab4b805edab39072f16c22e4b73dabaf66e316
Sam <Sam@notlikethis>
2018-09-28 14:20:41 +0200

    Fix script for fouriertrans

--- NewMediaCreate/example.m
+++ NewMediaCreate/example.m
@@ -1,5 +1,5 @@
 import fouriertrans.m.*
-load('ex.mat');     % Change according to exercise
+load('ex.mat');         %Change with test data
 
 figure(1), clf(1)
 subplot(2,2,1)
@@ -10,26 +10,23 @@ title('t, f')
 [w,F,ww,FF] = fouriertrans(t,f);
 subplot(2,2,2)
 plot(w, F)
-%axis([-0.5 ,5, 0, 1])     % change accordingly to data
+%axis([-0.5 ,15, 0, 5])     % change accordingly to data
 title('fouriertrans')
 
 
-clear max, clear index
 [maxVar1, maxi1] = max(F)
 xVal1 = w(maxi1);
 
-clear max, clear index
-[maxVar2, maxi2] = max(F(1:260)) %Change according to index of maxVar1
-xVal2 = w(maxi2);
+[maxVar2, maxi2] = max(F(50:end)) %Change according to index of maxVar1
+xVal2 = w(maxi2 + 50);            %Change as much as F(x:end)
 
-clear max, clear index
-[maxVar3, maxi3] = max(F) %Change according to index of maxVar2
-xVal3 = w(maxi3);
+[maxVar3, maxi3] = max(F(1000:end)) %Change according to index of maxVar2
+xVal3 = w(maxi3 + 1000);            %Change as much as F(x:end)
 
 
-xValuesOfPeaks = [xVal1, xVal2]
+xValuesOfPeaks = [xVal1, xVal2, xVal3]
 frequencies = 2.*pi./(1./xValuesOfPeaks)
-%frequencies = [12, 17];     % Change to find real values
+%frequencies = [0, 34, 78];     % Change to find real values
 
 
 [varred,a,b,err,pred] = sinus(t, f, frequencies);
--- NewMediaCreate/exer3.m
+++ NewMediaCreate/exer3.m
@@ -10,26 +10,26 @@ title('t, f')
 [w,F,ww,FF] = fouriertrans(t,f);
 subplot(2,2,2)
 plot(w, F)
-axis([-0.5 ,15, 0, 1])     % change accordingly to data
+axis([-0.5 ,15, 0, 5])     % change accordingly to data
 title('fouriertrans')
 
 
-clear max, clear index
+%clear max, clear index
 [maxVar1, maxi1] = max(F)
 xVal1 = w(maxi1);
 
-clear max, clear index
+%clear max, clear index
 [maxVar2, maxi2] = max(F(50:end)) %Change according to index of maxVar1
-xVal2 = w(maxi2);
+xVal2 = w(maxi2 + 50);            %Change as much as F(x:end)
 
-clear max, clear index
-[maxVar3, maxi3] = max(F(2:end)) %Change according to index of maxVar2
-xVal3 = w(maxi3);
+%clear max, clear index
+[maxVar3, maxi3] = max(F(1000:end)) %Change according to index of maxVar2
+xVal3 = w(maxi3 + 1000);            %Change as much as F(x:end)
 
 
 xValuesOfPeaks = [xVal1, xVal2, xVal3]
 frequencies = 2.*pi./(1./xValuesOfPeaks)
-frequencies = [0, 34, 78];     % Change to find real values
+%frequencies = [0, 34, 78];     % Change to find real values
 
 
 [varred,a,b,err,pred] = sinus(t, f, frequencies);
